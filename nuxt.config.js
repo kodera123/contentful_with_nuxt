c5f896cc5f4ec55305ee08be5cf2c4dbeb4e0d58
kodera123 <b.b.015.k.k@softbank.ne.jp>
2019-04-28 19:32:38 +0900

    somponent config 各ビューの用意

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,14 +1,22 @@
+const contentful = require('contentful')
+// ./nuxt.config.js
+const config = require('./.contentful.json')
+const client = contentful.createClient({
+  space: config.CTF_SPACE_ID,
+  accessToken: config.CTF_CDA_ACCESS_TOKEN
+})
+
+
 module.exports = {
   /*
   ** Headers of the page
   */
   head: {
-    title: 'test_nuxtblog',
     titleTemplate: '%s | Awesome JS SSR Blog',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'hogehoge' }
+      { hid: 'description', name: 'description', content: 'Nuxt.js and Contentful' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
@@ -17,6 +25,21 @@ module.exports = {
         href: 'https://cdnjs.cloudflare.com/ajax/libs/bulma/0.4.2/css/bulma.min.css'
       }
     ]
+  },
+  env: {
+    CTF_SPACE_ID: config.CTF_SPACE_ID,
+    CTF_CDA_ACCESS_TOKEN: config.CTF_CDA_ACCESS_TOKEN,
+    CTF_PERSON_ID: config.CTF_PERSON_ID,
+    CTF_BLOG_POST_TYPE_ID: config.CTF_BLOG_POST_TYPE_ID
+  },
+  generate: {
+    routes() {
+      return client.getEntries({
+        'content_type': config.CTF_BLOG_POST_TYPE_ID
+      }).then((entries) => {
+        return [...entries.items.map(entry => `posts/${entry.fields.slug}`)]
+      })
+    }
   },
   /*
   ** Customize the progress bar color
@@ -39,6 +62,10 @@ module.exports = {
         })
       }
     }
-  }
+  },
+  /*
+  ** plugins
+  */
+  plugins: ['./plugins/contentful.js']
 }
 
